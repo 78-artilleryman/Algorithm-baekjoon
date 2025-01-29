0249cb17aaf7db9f1f033cff555d12b0060f8944
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-01-30 00:24:42 +0900

    알고리즘수업-깊이우선탐색1

---
+++ DFS/24479.py
@@ -0,0 +1,30 @@
+import sys
+sys.setrecursionlimit(10 ** 6)
+input = sys.stdin.readline
+
+N, M, R = map(int, input().split())
+graph = [[] for _ in range(N+1)]
+visited = [0] * (N + 1)
+cnt = 1
+
+def dfs(graph, start, visited):
+    global cnt
+    visited[start] = cnt
+
+    for vv in graph[start]:
+        if visited[vv] == 0:
+            cnt += 1
+            dfs(graph, vv, visited)
+
+for i in range(M):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+for j in range(1, N+1):
+    graph[j].sort()
+
+dfs(graph, R, visited)
+
+for i in range(1, N+1):
+    print(visited[i])
